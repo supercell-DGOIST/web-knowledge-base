08172ae63eeca20c508cb6a6fb82d3f1beb5d0a8
唐晓铧 <supercell_t@outlook.com>
2023-04-09 16:17:15 +0800

    chore(other): 新增playwright做e2e测试

---
+++ playwright.config.ts
@@ -0,0 +1,90 @@
+import { defineConfig, devices } from '@playwright/test';
+
+const isCI = Boolean(process.env.CI);
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './e2e',
+  timeout: 30 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000,
+  },
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: isCI,
+  /* Retry on CI only */
+  retries: isCI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: isCI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'http://localhost:4040',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+
+    /* Only on CI systems run the tests headless */
+    headless: isCI,
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: isCI ? 'npm run preview' : 'npm run dev',
+    port: 4040,
+    reuseExistingServer: !isCI,
+  },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, type ServerOptions } from 'vite';
+import { env } from 'process';
 import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+
+const isTextEnv = env.NODE_ENV === 'test';
+
+const serverOption: ServerOptions = {
+  port: 4040,
+  open: true,
+};
+
+const setServer = (): ServerOptions => {
+  const server = {
+    cors: true,
+  };
+  return Object.assign(server, serverOption);
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,14 +38,6 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
-  preview: {
-    port: 4040,
-    open: true,
-  },
-  server: {
-    port: 4000,
-    open: true,
-    cors: true,
-    proxy: {},
-  },
+  preview: !isTextEnv ? serverOption : {},
+  server: !isTextEnv ? setServer() : {},
 });
